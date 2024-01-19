22e3d34b56ca0181e2d2f396e2423c5ac7ac5ea0
HRVB <h.boulangue@gmail.com>
2024-01-19 15:34:23 +0100

    Ajout du systeme de la gallerie elementBuilder() et displayProjectsGallery()

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -1,10 +1,7 @@
 import { removeCssClassOnNodeList, addCssClass } from "./utilities.js";
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
-// GALLERY SYSTEM
-
 let data; //console.log(data)
+const gallery = document.querySelector(".gallery-container");
 
 /**
  * This function get the data from the API.
@@ -13,7 +10,7 @@ let data; //console.log(data)
  * @param {String} resource the key word for the URL.
  * @returns data
  */
-export async function getData(resource) {
+async function getData(resource) {
     let fetchResource = resource;
 
     // Sets resource at init when arg:resource is undefined.
@@ -34,12 +31,72 @@ export async function getData(resource) {
         data = await response.json();
         console.log("DATA COME FROM API : " + fetchResource);
 
-        return console.log(data);
+        return data; 
+        // return console.log(data + fetchResource);
 
     } catch (error) {
         console.error(error);
     } 
 };
 
-getData("works");
-getData("categories");
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+// GALLERY SYSTEM
+
+/**
+ * This function builds each item of the gallery.
+ * 
+ * @param {String} title data.title - Sets figcaption of the figure element and the alt attribute of the img.
+ * @param {String} src  data.imageUrl - The URI of the figure element.
+ */
+async function elementBuilder(title, src) {
+    const galleryElement = document.createElement("figure");
+
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = title;
+
+    const imgElement = document.createElement("img");
+    imgElement.src = src;
+    imgElement.setAttribute("alt", `${titleElement}`);
+
+    galleryElement.appendChild(imgElement);
+    galleryElement.appendChild(titleElement);
+
+    if (gallery) {
+        gallery.appendChild(galleryElement);
+    };
+};
+
+/**
+ * This function displays all the projects of the gallery
+ * @param {Object} data the data object
+ * 
+ * [use getData("works") function].
+ * [use elementBuilder() function].
+ */
+async function displayProjectsGallery() {
+    data = await getData("works");
+
+    if (gallery) {
+        gallery.innerHTML = "";
+    };
+
+    for (let i =0; i < data.length; i++) {
+        elementBuilder(data[i].title, data[i].imageUrl);
+    };
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// APP INIT
+/**
+ * This function initializes the application :
+ * Retrieves data from API and displays it on the home page (const gallery).
+ */
+async function appInit() {
+    await getData();
+    await displayProjectsGallery();
+};
+///////////////////////////////////////////////////////////////////////
+appInit(); 
+///////////////////////////////////////////////////////////////////////
