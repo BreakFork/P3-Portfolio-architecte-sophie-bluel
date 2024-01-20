78e256cf775119a4e6f8d80cff1432463ca12141
HRVB <h.boulangue@gmail.com>
2024-01-20 20:48:06 +0100

    Ajout de fonction dans utilities.js pour edition.js

--- FrontEnd/assets/js/utilities.js
+++ FrontEnd/assets/js/utilities.js
@@ -1,15 +1,14 @@
 // ///////////////////////////////////////////////////////////////////////
 // UTILITIES FUNCTIONS ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////
-// FILTERS SYSTEM
-
 /**
- * This function removes one CSS class on nodeList.
+ * This function removes one CSS class on a nodeList.
+ * 
+ * [used in filterEngine()]
  * 
  * @param {String} removedClass the CSS class to remove.
  * @param {String} classTargetingNodeList the CSS class that targets the node list.
+ * 
  */
 export async function removeCssClassOnNodeList(removedClass, classTargetingNodeList) {
     const nodeList = [...document.getElementsByClassName(classTargetingNodeList)];
@@ -21,15 +20,30 @@ export async function removeCssClassOnNodeList(removedClass, classTargetingNodeL
     })
 };
 
-//////////////////////////////////////////////////////////////////////////
-// APP
-
 /**
  * This function adds a CSS class on a HTMLElement.
  * 
+ * [used in filterEngine()]
+ * 
  * @param {String} cssClass The CSS class to add on the element.
  * @param {Element} element The HTMLElement receving the CSS class.
+ * 
  */
 export async function addCssClass(cssClass, element) {
     element.classList.add(cssClass);
+};
+
+/**
+ * This function toggles a CSS class on a HTMLElement.
+ * 
+ * [used in toggleEditionHeaderDisplay()]
+ * [used in toggleFilterButtonsDisplay()]
+ * [used in toggleEditorButtonDisplay()]
+ * 
+ * @param {String} cssClass The CSS class to switch.
+ * @param {Element} element The HTMLElement on wich we switch the CSS class.
+ * 
+ */
+export async function toggleCssClass(cssClass, element) {
+    element.classList.toggle(cssClass);
 };
